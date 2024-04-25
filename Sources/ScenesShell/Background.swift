4e5e7f930691ab5ab96d2f23c1662c5cfb7f2733
AI-RobotMaster <kris.smpt@gmail.com>
2024-04-25 12:42:23 -0500

    added background

--- Sources/ScenesShell/Background.swift
+++ Sources/ScenesShell/Background.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Scenes
 import Igis
 
@@ -7,9 +8,25 @@ import Igis
 
 
 class Background : RenderableEntity {
+    let imageURL = "https://codermerlin.academy/users/krishna-shah/images/usmap.svg"
+    let usmap : Image
+    init() {
+        // Using a meaningful name can be helpful for debugging
+        guard let usmapURL = URL(string:imageURL) else {
+            fatalError("Failed to create URL for image")
+        }
+        usmap = Image(sourceURL:usmapURL)
+        super.init(name:"Background")
+    }
 
-      init() {
-          // Using a meaningful name can be helpful for debugging
-          super.init(name:"Background")
-      }
+    override func setup(canvasSize: Size, canvas: Canvas) {
+        usmap.renderMode = .destinationRect(Rect(topLeft:Point(), size:canvasSize))
+        canvas.setup(usmap)
+    }
+
+    override func render(canvas: Canvas) {
+        if usmap.isReady {
+            canvas.render(usmap)
+        }
+    }
 }
